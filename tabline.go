3b505cc74293fe36388c2367062c6a192fdaf414
Dongdong Zhou <dzhou121@gmail.com>
2017-05-12 08:56:07 +0100

    tabline refactor

--- tabline.go
+++ tabline.go
@@ -19,12 +19,11 @@ type Tabline struct {
 
 // Tab in the tabline
 type Tab struct {
+	SpanHandler
 	box      *ui.Box
 	ID       int
 	Name     string
 	current  bool
-	span     *SpanHandler
-	bg       *SpanHandler
 	width    int
 	chars    int
 	cross    *Svg
@@ -63,57 +62,45 @@ func (t *Tabline) update(args []interface{}) {
 			continue
 		}
 		if i > len(t.Tabs)-1 {
-			box := ui.NewHorizontalBox()
-			handler := &SpanHandler{}
-			tabSpan := ui.NewArea(handler)
-			handler.area = tabSpan
-			bgHandler := &SpanHandler{}
-			bgSpan := ui.NewArea(bgHandler)
-			bgHandler.area = bgSpan
-			padding := (t.height - editor.font.height - 2) / 2
-			paddingLeft := editor.font.width * 2
 			chars := 21
 			fileiconWidth := editor.font.width * 2
+			padding := (t.height - editor.font.height - 2) / 2
+			paddingLeft := editor.font.width * 2
 			cross := newSvg("cross", editor.font.width*2, editor.font.width*2, newRGBA(255, 255, 255, 1), newRGBA(0, 0, 0, 1))
 			fileicon := newSvg("default", fileiconWidth, fileiconWidth, nil, nil)
+
+			box := ui.NewHorizontalBox()
 			tab := &Tab{
 				box:      box,
-				span:     handler,
 				width:    editor.font.width*chars + fileiconWidth + 3*paddingLeft + editor.font.width,
 				chars:    chars,
-				bg:       bgHandler,
 				cross:    cross,
 				fileicon: fileicon,
 			}
+			tab.area = ui.NewArea(tab)
+			tab.font = editor.font
+			tab.setSize(tab.width, t.height)
+			tab.paddingTop = padding
+			tab.paddingLeft = paddingLeft + fileiconWidth + editor.font.width
 			t.Tabs = append(t.Tabs, tab)
-			fg := newRGBA(212, 215, 214, 1)
-			handler.SetColor(fg)
-			handler.SetBackground(newRGBA(0, 0, 0, 1))
-			handler.SetFont(editor.font)
-			handler.setSize(tab.width-paddingLeft*3, t.height-2)
 			box.SetSize(tab.width, t.height)
-			bgHandler.setSize(tab.width, t.height)
-			bgHandler.SetBackground(newRGBA(0, 0, 0, 1))
-			handler.paddingTop = padding
-			bgHandler.borderRight = &Border{
+			tab.borderRight = &Border{
 				width: 1,
 				color: newRGBA(0, 0, 0, 1),
 			}
-			bgHandler.borderBottom = &Border{
+			tab.borderBottom = &Border{
 				width: 2,
 				color: newRGBA(0, 0, 0, 1),
 			}
 
 			ui.QueueMain(func() {
-				box.Append(bgSpan, false)
-				box.Append(tabSpan, false)
+				box.Append(tab.area, false)
 				box.Append(cross.area, false)
 				box.Append(fileicon.area, false)
 				t.box.Append(box, false)
 				box.SetPosition(i*tab.width, 0)
 				cross.area.SetPosition(tab.width-paddingLeft-editor.font.width, (t.height-cross.height)/2)
 				fileicon.setPosition(paddingLeft, (t.height-fileicon.height)/2)
-				tabSpan.SetPosition(paddingLeft+fileiconWidth+editor.font.width, 0)
 			})
 		}
 		tab := t.Tabs[i]
@@ -126,28 +113,25 @@ func (t *Tabline) update(args []interface{}) {
 			text = text[len(text)-tab.chars+3 : len(text)]
 			text = "..." + text
 		}
-		tab.span.SetText(text)
+		tab.text = text
 		if tab.ID == t.CurrentID {
 			tab.current = true
-			tab.bg.borderBottom.color = newRGBA(81, 154, 186, 1)
-			tab.bg.SetBackground(newRGBA(0, 0, 0, 1))
-			tab.span.SetBackground(newRGBA(0, 0, 0, 1))
-			tab.span.SetColor(newRGBA(212, 215, 214, 1))
+			tab.borderBottom.color = newRGBA(81, 154, 186, 1)
+			tab.bg = newRGBA(0, 0, 0, 1)
+			tab.color = newRGBA(212, 215, 214, 1)
 			tab.cross.color = newRGBA(212, 215, 214, 1)
 			tab.cross.bg = newRGBA(0, 0, 0, 1)
 		} else {
 			tab.current = false
-			tab.bg.borderBottom.color = newRGBA(0, 0, 0, 1)
-			tab.bg.SetBackground(newRGBA(24, 29, 34, 1))
-			tab.span.SetBackground(newRGBA(24, 29, 34, 1))
-			tab.span.SetColor(&editor.Foreground)
+			tab.borderBottom.color = newRGBA(0, 0, 0, 1)
+			tab.bg = newRGBA(24, 29, 34, 1)
+			tab.color = &editor.Foreground
 			tab.cross.color = &editor.Foreground
 			tab.cross.bg = newRGBA(24, 29, 34, 1)
 		}
 		ui.QueueMain(func() {
 			tab.box.Show()
-			tab.span.area.QueueRedrawAll()
-			tab.bg.area.QueueRedrawAll()
+			tab.area.QueueRedrawAll()
 			tab.cross.area.QueueRedrawAll()
 			tab.fileicon.area.QueueRedrawAll()
 		})
