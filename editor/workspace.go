7e19ebcf911181d3a431e9b0eee41ef1d8ad8dfa
akiyosi <akiyoshi.maruyama@gmail.com>
2018-07-11 21:41:02 +0900

    Fix the deadlock between autocmd and waiting input for external cmd.

--- editor/workspace.go
+++ editor/workspace.go
@@ -636,7 +636,7 @@ func (w *Workspace) handleRPCGui(updates []interface{}) {
 	case "signature_hide":
 		w.signature.hide()
 	case "gonvim_copy_clipboard":
-		editor.copyClipBoard()
+		go editor.copyClipBoard()
 	case "gonvim_workspace_new":
 		editor.workspaceNew()
 	case "gonvim_workspace_next":
@@ -646,16 +646,18 @@ func (w *Workspace) handleRPCGui(updates []interface{}) {
 	case "gonvim_workspace_switch":
 		editor.workspaceSwitch(reflectToInt(updates[1]))
 	case "gonvim_workspace_cwd":
-		w.setCwd()
+		go w.setCwd()
 	case "gonvim_workspace_redrawSideItem":
+	 go func() {
 		fl := editor.wsSide.items[editor.active].Filelist
 		if fl.active != -1 {
 			if fl.Fileitems != nil {
 				fl.Fileitems[fl.active].updateModifiedbadge()
 			}
 		}
+	 }()
 	case "gonvim_workspace_redrawSideItems":
-		editor.wsSide.items[editor.active].setCurrentFileLabel()
+		go editor.wsSide.items[editor.active].setCurrentFileLabel()
 	case GonvimMarkdownNewBufferEvent:
 		go w.markdown.newBuffer()
 	case GonvimMarkdownUpdateEvent:
