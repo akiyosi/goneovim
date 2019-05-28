31b9abdf3e90c5119b9c1639c15e337c0a6e379a
akiyosi <akiyoshi.maruyama@gmail.com>
2019-05-28 20:48:25 +0900

    Fix cmdline cursor position while processing IME event

--- editor/palette.go
+++ editor/palette.go
@@ -258,10 +258,8 @@ func (p *Palette) setPattern(text string) {
 }
 
 func (p *Palette) cursorMove(x int) {
-	//p.cursorX = int(p.ws.font.defaultFontMetrics.Width(string(p.patternText[:x])))
 	font := gui.NewQFontMetricsF(gui.NewQFont2(editor.config.Editor.FontFamily, editor.config.Editor.FontSize, 1, false))
 	p.cursorX = int(font.HorizontalAdvance(string(p.patternText[:x]), -1))
-	// p.cursor.Move2(p.cursorX+p.patternPadding, p.patternPadding)
 	p.ws.cursor.x = p.cursorX+p.patternPadding
 	p.ws.cursor.y = p.patternPadding+p.ws.cursor.shift
 	p.ws.cursor.widget.Move2(p.ws.cursor.x, p.ws.cursor.y)
