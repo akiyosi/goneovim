044dd280c26042dcc18a10cb45000ceae5a20aa3
akiyosi <akiyoshi.maruyama@gmail.com>
2020-09-10 22:09:47 +0900

    logical revert of 9949899 and no need to get pumheight

--- editor/workspace.go
+++ editor/workspace.go
@@ -463,7 +463,7 @@ func (w *Workspace) initGonvim() {
 	au GonvimAu VimEnter * call rpcnotify(1, "Gui", "gonvim_enter", getcwd())
 	au GonvimAu BufEnter * call rpcnotify(0, "Gui", "gonvim_bufenter")
 	au GonvimAu WinEnter,FileType * call rpcnotify(0, "Gui", "gonvim_filetype", &ft, win_getid())
-	au GonvimAu OptionSet * if (&ro != 1 && &modeline != 1) | call rpcnotify(0, "Gui", "gonvim_optionset") | endif
+	au GonvimAu OptionSet * if &ro != 1 | call rpcnotify(0, "Gui", "gonvim_optionset") | endif
 	au GonvimAu TermEnter * call rpcnotify(0, "Gui", "gonvim_termenter")
 	au GonvimAu TermLeave * call rpcnotify(0, "Gui", "gonvim_termleave")
 	aug GonvimAuWorkspace | au! | aug END
@@ -1777,7 +1777,7 @@ func (w *Workspace) bufEnter() {
 func (w *Workspace) optionSet() {
 	w.optionsetMutex.Lock()
 	w.getTabStop()
-	w.getPumHeight()
+	// w.getPumHeight()
 	w.optionsetMutex.Unlock()
 	// w.getFileType()
 }
