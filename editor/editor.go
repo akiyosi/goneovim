86678c52038a43b0ac1a887447173182f52fd683
akiyosi <akiyoshi.maruyama@gmail.com>
2023-11-02 22:41:48 +0900

    Use QRawFont to drawing text

--- editor/editor.go
+++ editor/editor.go
@@ -268,6 +268,23 @@ func InitEditor(options Options, args []string) {
 
 	// e.setAppDirPath(home)
 
+	loadSysFontErr := <-e.loadSysFontCh
+	if loadSysFontErr != nil {
+		panic(loadSysFontErr)
+	}
+
+	e.fontCh = make(chan []*Font, 2)
+	go func() {
+		e.fontCh <- parseFont(
+			e.config.Editor.FontFamily,
+			e.config.Editor.FontSize,
+			e.config.Editor.FontWeight,
+			e.config.Editor.FontStretch,
+			e.config.Editor.Linespace,
+			e.config.Editor.Letterspace,
+		)
+	}()
+
 	e.initSVGS()
 
 	e.initColorPalette()
