012b764d3602578879cd2f736ba612f401537e6f
akiyosi <akiyoshi.maruyama@gmail.com>
2024-01-04 14:42:45 +0900

    Add support forward and back mouse buttons

--- editor/window.go
+++ editor/window.go
@@ -3358,6 +3358,10 @@ func (w *Window) mouseEvent(event *gui.QMouseEvent) {
 			bt = core.Qt__RightButton
 		} else if event.Buttons()&core.Qt__MidButton > 0 {
 			bt = core.Qt__MidButton
+		} else if event.Buttons()&core.Qt__XButton1 > 0 {
+			bt = core.Qt__XButton1
+		} else if event.Buttons()&core.Qt__XButton2 > 0 {
+			bt = core.Qt__XButton2
 		}
 	}
 
@@ -3369,6 +3373,10 @@ func (w *Window) mouseEvent(event *gui.QMouseEvent) {
 		button += "right"
 	case core.Qt__MidButton:
 		button += "middle"
+	case core.Qt__XButton1:
+		button += "x1"
+	case core.Qt__XButton2:
+		button += "x2"
 	case core.Qt__NoButton:
 	default:
 	}
@@ -3408,6 +3416,7 @@ func (w *Window) mouseEvent(event *gui.QMouseEvent) {
 	w.lastMouseEvent.row = row
 	w.lastMouseEvent.col = col
 
+	editor.putLog("mouse event:", button, action, mod, w.grid, row, col)
 	w.s.ws.nvim.InputMouse(button, action, mod, w.grid, row, col)
 }
 
